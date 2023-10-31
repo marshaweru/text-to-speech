9fbe3f97264867310df4dbdda57aee1154f82c47
weru <147871343+marshaweru@users.noreply.github.com>
2023-10-31 06:40:05 -0700

    Display voices in dropdown

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 //Add one variable
 let speech = new SpeechSynthesisUtterance();
 
-//Switch voices
+//Switch voices by adding one variable
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();//Will provide all voices available on device
+    speech.voice = voices[0];//By default it will speak in first voice 
+    //Display voices in dropdown
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+}
 
 document.querySelector("button").addEventListener("click", () =>{
     speech.text = document.querySelector("textarea").value;
